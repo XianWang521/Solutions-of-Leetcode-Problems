89686e27a3ac2c4ae5f0be57e4e056b6006cd6ce
xianwang <wxwjj521804@gmail.com>
2020-08-04 23:44:35 +0800

    2020.8.04

--- 12.整数转罗马数字.cpp
+++ 12.整数转罗马数字.cpp
@@ -8,7 +8,20 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        
+        string roman="";
+        string re[4][10]={
+            {"0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"},
+            {"0", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"},
+            {"0", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"},
+            {"0", "M", "MM", "MMM"}};
+        int i=0,k=1000;
+        for(i=3;i>=0;i--)
+        {
+            if(num/k!=0) roman+=re[i][num/k];
+            num%=k;
+            k/=10;
+        }
+        return roman;
     }
 };
 // @lc code=end

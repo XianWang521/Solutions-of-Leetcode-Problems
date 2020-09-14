9ffeb1d16f792431a25d17e82549787fa6b61dd1
xianwang <wxwjj521804@gmail.com>
2020-09-14 19:51:27 +0800

    2020.9.14

--- 29.两数相除.java
+++ 29.两数相除.java
@@ -7,7 +7,23 @@
 // @lc code=start
 class Solution {
     public int divide(int dividend, int divisor) {
-
+        if(dividend==Integer.MIN_VALUE && divisor==-1)
+            return Integer.MAX_VALUE;
+        
+        int result = 0, flag = (dividend>0&&divisor>0) || (dividend<0&&divisor<0)?1:-1;
+		dividend = -Math.abs(dividend);
+        divisor = -Math.abs(divisor);
+        
+        while(dividend <= divisor) {
+			int temp = divisor, times = 1;
+			while(dividend-temp <= temp) {
+                times = times<<1;
+                temp = temp<<1;
+			}
+			dividend -= temp;
+			result += times;
+        }
+        return result*flag;
     }
 }
 // @lc code=end

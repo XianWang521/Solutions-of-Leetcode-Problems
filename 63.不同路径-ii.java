304411f81480781503dd83065a4df5bb4e33e36d
xianwang <wxwjj521804@gmail.com>
2021-06-08 14:29:01 +0800

    2021.6.08

---
+++ 63.不同路径-ii.java
@@ -0,0 +1,28 @@
+import javax.crypto.spec.IvParameterSpec;
+
+/*
+ * @lc app=leetcode.cn id=63 lang=java
+ *
+ * [63] 不同路径 II
+ */
+
+// @lc code=start
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int[][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
+        dp[0][0] = obstacleGrid[0][0] == 1? 0: 1;
+        if(dp[0][0] == 0) return 0;
+
+        for(int i = 0; i < obstacleGrid.length; i++){
+            for(int j = 0; j < obstacleGrid[0].length; j++){
+                if(obstacleGrid[i][j] == 1) dp[i][j] = 0;
+                else if(i == 0 && j > 0) dp[i][j] = dp[i][j-1];
+                else if(j == 0 && i > 0) dp[i][j] = dp[i-1][j];
+                else if(i > 0 && j > 0) dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[obstacleGrid.length-1][obstacleGrid[0].length-1];
+    }
+}
+// @lc code=end
+

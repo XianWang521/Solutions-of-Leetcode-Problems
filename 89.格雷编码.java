cc9d267d41b806a6768cd74a8d793636dfbe225f
xianwang <wxwjj521804@gmail.com>
2021-06-16 14:57:37 +0800

    2021.6.16

---
+++ 89.格雷编码.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=89 lang=java
+ *
+ * [89] 格雷编码
+ */
+
+// @lc code=start
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> res = new ArrayList<>();
+        res.add(0);
+        for(int i = 0; i < n; i++)
+            for(int j = res.size()-1; j >= 0; j--)
+                res.add(res.get(j) ^ (1<<i));
+        return res;
+    }
+}
+// @lc code=end
+

8d36a8ae9243513dd2071e9ebbe3d0426e1ebb18
xianwang <wxwjj521804@gmail.com>
2021-06-18 21:03:22 +0800

    2021.6.18

--- 95.不同的二叉搜索树-ii.java
+++ 95.不同的二叉搜索树-ii.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 /*
  * @lc app=leetcode.cn id=95 lang=java
  *
@@ -22,7 +24,23 @@
  */
 class Solution {
     public List<TreeNode> generateTrees(int n) {
+        return generate(1, n);
+    }
+    public List<TreeNode> generate(int start, int end){
+        List<TreeNode> res = new LinkedList<>();
+        if(start > end){
+            res.add(null);
+            return res;
+        }
 
+        for(int i = start; i <= end; i++){
+            List<TreeNode> l = generate(start, i-1);
+            List<TreeNode> r = generate(i+1, end);
+            for(TreeNode left : l)
+                for(TreeNode right : r)
+                    res.add(new TreeNode(i, left, right));
+        }
+        return res;
     }
 }
 // @lc code=end

ae754cb33d758603b5903258b6f08c75ff354059
xianwang <wxwjj521804@gmail.com>
2021-07-14 15:14:24 +0800

    2021.7.14

--- 148.排序链表.java
+++ 148.排序链表.java
@@ -17,7 +17,30 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-
+        if(head == null || head.next == null) return head;
+        ListNode slow = head, fast = head.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode temp = slow.next;
+        slow.next = null;
+        ListNode l = sortList(head),r = sortList(temp), dummy = new ListNode();
+        temp = dummy;
+        while(l != null && r != null){
+            if(l.val < r.val){
+                temp.next = l;
+                l = l.next;
+            }
+            else{
+                temp.next = r;
+                r = r.next;
+            }
+            temp = temp.next;
+            if(l != null) temp.next = l;
+            else if(r != null) temp.next = r;
+        }
+        return dummy.next;
     }
 }
 // @lc code=end

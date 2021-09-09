09f0ffed4d7130e83348ee45cf545a203effeeb0
xianwang <wxwjj521804@gmail.com>
2021-09-09 17:24:48 +0800

    2021.9.09

--- 229.求众数-ii.java
+++ 229.求众数-ii.java
@@ -11,6 +11,42 @@ import java.util.Map;
 // @lc code=start
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        if(nums.length == 0) return res;
+
+        int a = nums[0], b = nums[0], acount = 0, bcount = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == a) acount++;
+            else if(nums[i] == b) bcount++;
+            if(nums[i] != a && nums[i] != b){  
+                if(acount == 0){
+                    a = nums[i];
+                    acount = 1;
+                    continue;
+                }
+                else if(bcount == 0){
+                    b = nums[i];
+                    bcount = 1;
+                    continue;
+                }
+                acount--;
+                bcount--;
+            }
+        }
+
+        acount = bcount = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == a) acount++;
+            else if(nums[i] == b) bcount++;
+        }
+        if(acount > nums.length / 3) res.add(a);
+        if(bcount > nums.length / 3) res.add(b);
+        return res;
+    }
+}
+/*
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         for(int i = 0; i < nums.length; i++)
             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
@@ -20,5 +56,6 @@ class Solution {
         return res;
     }
 }
+*/
 // @lc code=end
 

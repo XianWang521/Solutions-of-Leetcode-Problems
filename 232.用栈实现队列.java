8e0cdd71f7a7fc85fbf60bc1e7c3698406151949
xianwang <wxwjj521804@gmail.com>
2021-09-15 14:58:15 +0800

    2021.9.15

--- 232.用栈实现队列.java
+++ 232.用栈实现队列.java
@@ -1,3 +1,6 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
 /*
  * @lc app=leetcode.cn id=232 lang=java
  *
@@ -6,30 +9,36 @@
 
 // @lc code=start
 class MyQueue {
-
+    Deque<Integer> in;
+    Deque<Integer> out;
     /** Initialize your data structure here. */
     public MyQueue() {
-
+        in = new LinkedList<>();
+        out = new LinkedList<>();
     }
     
     /** Push element x to the back of queue. */
     public void push(int x) {
-
+        in.push(x);
     }
     
     /** Removes the element from in front of queue and returns that element. */
     public int pop() {
-
+        if(out.isEmpty())
+            while(!in.isEmpty()) out.push(in.pop());
+        return out.pop();
     }
     
     /** Get the front element. */
     public int peek() {
-
+        if(out.isEmpty())
+            while(!in.isEmpty()) out.push(in.pop());
+        return out.peek();
     }
     
     /** Returns whether the queue is empty. */
     public boolean empty() {
-
+        return in.isEmpty() && out.isEmpty();
     }
 }
 

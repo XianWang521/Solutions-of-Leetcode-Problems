28339217c91cef4ef979af6403d20340f2d958a3
xianwang <wxwjj521804@gmail.com>
2021-10-16 00:47:51 +0800

    2021.10.16

---
+++ 326.3-的幂.java
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=326 lang=java
+ *
+ * [326] 3的幂
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        while(n > 0 && n % 3 == 0) n /= 3;
+        return n == 1;
+    }
+}
+// @lc code=end
+
